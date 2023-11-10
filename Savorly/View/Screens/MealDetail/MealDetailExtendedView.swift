f636fdf25b25ab95ee0bb7c001458a9cfbe5e159
Sreejith <sreejithakamenon@gmail.com>
2023-11-10 12:07:41 -0500

    Added App Icon

---
+++ Savorly/View/Screens/MealDetail/MealDetailExtendedView.swift
@@ -0,0 +1,122 @@
+//
+//  MealDetailExtendedView.swift
+//  Savorly
+//
+//  Created by Sreejith K Menon on 08/11/23.
+//
+
+import SwiftUI
+import CachedAsyncImage
+
+struct MealDetailExtendedView: View {
+    
+    let meal: MealDetail
+    let ingredients: [Ingredients]
+    
+    let imageSize: CGFloat = AppConstants.UIUtils.mealDetailExtendedImageSize
+    
+    var body: some View {
+        
+        ScrollView {
+            
+            VStack(alignment: .leading) {
+                
+                HStack {
+                    if meal.strMealThumb != nil {
+                        CachedAsyncImage(url: URL(string: meal.strMealThumb!)) { phase in
+                            if let image = phase.image {
+                                image.resizable()
+                                    .scaledToFill()
+                                    .frame(width: imageSize, height: imageSize)
+                                    .clipped()
+                                
+                            } else if phase.error != nil {
+                                
+                                Image(systemName: AppConstants.UIUtils.noImageSymbol)
+                                    .font(.largeTitle)
+                                    .frame(width: imageSize, height: imageSize)
+                                
+                            } else {
+                                ProgressView()
+                                    .frame(width: imageSize, height: imageSize)
+                            }
+                        }
+                        .cornerRadius(AppConstants.UIUtils.cachedAsyncImageMealDetailCornerRadius)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: AppConstants.UIUtils.cachedAsyncImageMealDetailCornerRadius)
+                                .stroke(Color.init(hex: AppConstants.UIUtils.imageBorderHexColor), lineWidth: AppConstants.UIUtils.cachedAsyncImageMealDetailCornerLineWidth)
+                        )
+                    } else {
+                        Color.gray.frame(width: imageSize, height: imageSize)
+                    }
+                    
+                    VStack {
+                        Text(meal.strMeal!)
+                            .bold()
+                            .font(.title2)
+                            .padding()
+                        
+                        Spacer()
+                    }
+                    
+                    
+                    
+                    Spacer()
+                }
+                
+                Divider()
+                    .padding(.top)
+                    .padding(.bottom)
+                
+                if meal.strInstructions != nil {
+                    
+                    HStack {
+                        Text(AppConstants.UIUtils.instructionsMealTitle)
+                            .font(.title2)
+                            .bold()
+                        
+                        Spacer()
+                    }
+                    
+                    Text(meal.strInstructions!)
+                        .padding(.top, AppConstants.UIUtils.instructionsMealDetailTextPaddingTop)
+                }
+                
+                Divider()
+                    .padding(.top)
+                    .padding(.bottom)
+                
+                if ingredients.count > 0 {
+                    
+                    VStack(alignment: .leading) {
+                        
+                        Text(AppConstants.UIUtils.ingredientsMealTitle)
+                            .font(.title2)
+                            .bold()
+                            .padding(.bottom, AppConstants.UIUtils.ingredientsMealDetailTitlePaddingBottom)
+                        
+                        ForEach(ingredients) { (item: Ingredients) in
+                            HStack {
+                                Text(item.ingredient)
+                                
+                                Text(AppConstants.UIUtils.ingredientsMealSeparatorSymbol)
+                                
+                                Text(item.measure)
+                            }
+                        }
+                        
+                    }
+                }
+            }
+            
+        }
+        .padding(.leading)
+        .padding(.trailing)
+    }
+}
+
+struct MealDetailExtendedView_Previews: PreviewProvider {
+    static var previews: some View {
+        MealDetailExtendedView(meal: MealDetail.example1(), ingredients: MealDetail.example3())
+    }
+}
